034919c487fdae7796a94b716452204d4b27a3e3
vagner bernal <vagner_bcastro@hotmail.com>
2013-08-14 16:59:20 -0300

    Alteracao nas configuracoes para habilitar a Controller TarefaController

--- CrudAngular/src/main/java/com/eits/crudangular/controller/TarefasController.java
+++ CrudAngular/src/main/java/com/eits/crudangular/controller/TarefasController.java
@@ -4,12 +4,17 @@ import java.util.List;
 
 
 
+
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.eits.crudangular.model.Tarefa;
@@ -24,15 +29,23 @@ public class TarefasController {
 	TarefaDao dao;
 	
 	
+	
+	@RequestMapping(value="/home")
+	public String home() {
+		return "index";
+	}
+	
+	
+	
 	@Transactional
-	@RequestMapping("adicionaTarefa")
+	@RequestMapping(value="adicionaTarefa", method=RequestMethod.POST)
 	public void adiciona(@RequestBody Tarefa tarefa) {
 		dao.adiciona(tarefa);
 	}
 	
 	
 	@ResponseBody
-	@RequestMapping("listaTarefas")
+	@RequestMapping(value = "/listaTarefas", method=RequestMethod.GET)
 	public List<Tarefa> lista() {
 		return dao.lista();
 	}
